dbacfd904dd1f5ca9b4b84ea8b490c1504d9ff6a
Ooftaco <nyssagandhi85@gmail.com>
2022-06-25 09:57:11 -0400

    pipes can rotate, but still need correct position tester

--- Assets/Scripts/Tasks/PipeRotate.cs
+++ Assets/Scripts/Tasks/PipeRotate.cs
@@ -5,12 +5,49 @@ using Unity.UI;
 
 public class PipeRotate : Task
 {
-    
+public GameObject[] pipes;
+int rotations = 0;
+
+public void Start()
+{
+    RandomizeRotations();
+
+}
+
+
    public void RotatePipe( GameObject button)
    {
     button.transform.Rotate(new Vector3(0,0,90));
-
+    rotations = (rotations + 1) % 3;
+    if (CheckRotations())
+    {
+        CloseUI();
+    }
+    Debug.Log("rotations " + rotations);
 
    }
+
+   public void RandomizeRotations ()
+   {
+    for (int j = 0; j< 6; j++)
+    {
+        RotatePipe( pipes[j]);
+    }
+    
+   }
+
+   public bool CheckRotations()
+   {
+    foreach ( GameObject pipe in pipes)
+    {
+        if ( (rotations % 3) == 0 )
+        {
+            return true;
+        }
+    }
+    return false;
+   }
+
+
    
 }
