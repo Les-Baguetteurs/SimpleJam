da70e08c0a89b325125b0103e8e784829e9aec07
TreyC4 <85621991+TreyC4@users.noreply.github.com>
2022-06-26 00:55:03 -0400

    UI

---
+++ Assets/Menu.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+public class Menu : MonoBehaviour
+{
+    public static bool isPaused = false;  
+    public GameObject PauseMenu;  
+    public GameObject CanvasMenu;  
+    //public GameObject Canvas;  
+    CanvasGroup Canvas;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        isPaused = true;  
+        Canvas = GetComponent<CanvasGroup>();
+        PauseMenu.transform.localScale = new Vector3(0, 0, 0);  
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))  
+        {
+            if (isPaused) {
+                Resume();  
+            }
+            else {
+                Pause();  
+            }
+        }
+    }
+    public void pause(){
+        isPaused = true;
+    }
+    public void Resume(){
+        Canvas.alpha = 0f;  
+        Canvas.blocksRaycasts = false;  
+        PauseMenu.transform.localScale = new Vector3(0, 0, 0);  
+        Time.timeScale = 1f;  
+        isPaused = false;  
+          
+    }  
+    public void Pause(){
+        Canvas.alpha = 1f;  
+        Canvas.blocksRaycasts = true;  
+        PauseMenu.transform.localScale = new Vector3(1, 1, 1);
+        Time.timeScale = 0f;  
+        isPaused = true;
+    }
+    
+
+    public void MainMenu(){ 
+        SceneManager.LoadScene(0);
+    }
+    public void quitgame(){
+        Application.Quit();  
+    }
+    
+    
+}
